5d114e98b837210b3a5225a0b9025ec835c0b315
Zack Hartmann <zackhartmann@gmail.com>
2017-10-25 21:06:05 +0000

    notations

--- Pset1/hello.c
+++ Pset1/hello.c
@@ -1,5 +1,8 @@
+// includes the standard input and output header file library (for printf)
 #include <stdio.h>
 
+// declares the main method
 int main(void) {
+    // print formatted text
 	printf("Hello, World!");
 }
