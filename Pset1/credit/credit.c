2adae680c814d65ce9602e88b8817b70f8fe6007
Zack Hartmann <zackhartmann@gmail.com>
2017-10-29 19:54:55 +0000

    Flip Parity

--- Pset1/credit/credit.c
+++ Pset1/credit/credit.c
@@ -17,30 +17,37 @@ int main(void) {
     plastic = 378282246310005;
     long long remain;
     int sum = 0;
-    printf("%s%lld\n\n", "INITIAL:\n", plastic);
+    // string card = "INVALID";
+    bool even = true;
 
-    while(plastic > 1){
-        // EVENS //
-        // using modulo thusly will render the last digit
-        sum += plastic % 10;
+    while(plastic > 0){
+        // the if statement makes it only process even or odd
+        if (even) {
+            // EVENS //
+            // using modulo thusly will render the last digit
+            sum += plastic % 10;
+        } else {
+            // ODDS //
+            // multiply product by two in accordance with Luhn's algorithm
+            remain = (plastic % 10) * 2;
+            // Luhn requires double digit numbers to be accounted for specially
+            if (remain > 9) {
+                // a number like 14 becomes a 1 and a 4.
+                // it can never be more than 18 (2x of 9), so we can just add a 1.
+                sum += remain % 10 + 1;
+            } else {
+                sum += remain;
+            }
+        }
+        // flip parity
+        even = !even;
+
         // cleave away the last digit
         plastic = plastic / 10;
-
-        // ODDS //
-        // multiply product by two in accordance with Luhn's algorithm
-        remain = (plastic % 10) * 2;
-        // cleave away the last digit again
-        plastic = plastic / 10;
-        // Luhn requires double digit numbers to be accounted for specially
-        if (remain > 9) {
-            // use modulo to cleave apart the second digit
-            // a number like 14 becomes a 1 and a 4.
-            // it can never be more than 18 (2x of 9), so we can just add a 1.
-            sum += remain % 10 + 1;
-        } else {
-            sum += remain;
-        }
     }
 
     printf("%s%d\n", "Sum: ", sum);
+
+    // they instructed me to return 0
+    return 0;
 }
