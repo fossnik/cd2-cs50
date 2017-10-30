77595325c590db2b69ad00e8731312a26eab3772
Zack Hartmann <zackhartmann@gmail.com>
2017-10-30 22:46:00 +0000

    check50 - pass

--- Pset2/initials/initials.c
+++ Pset2/initials/initials.c
@@ -1,4 +1,4 @@
-// Credit
+// initials
 
 #include <stdio.h>
 #include <cs50.h>
@@ -6,12 +6,11 @@
 #include <ctype.h> // for toupper
 
 int main(void) {
-    // string name = get_string();
-    string name = "wolfgang amadeus mozart";
+    string name = get_string();
 
     // verify that input is not null
     if (name != NULL) {
-        // establishes demarcation between names
+        // establishes demarcation between names (from spaces)
         bool spacetrip = true;
         // loop through each letter of input
         for (int i = 0, length = strlen(name); i < length; i++) {
@@ -20,6 +19,7 @@ int main(void) {
                 spacetrip = false;
             }
             // if it's a space then the next character is an initial
+            // 32 is the ASCII for a space.
             if ((int) name[i] == 32) {
                 spacetrip = true;
             }
