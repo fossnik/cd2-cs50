c77d7c781e92b750f660fc83967b7ea007233f99
Zack Hartmann <zackhartmann@gmail.com>
2017-10-30 22:42:51 +0000

    Working Prototype!

--- Pset2/initials/initials.c
+++ Pset2/initials/initials.c
@@ -3,9 +3,28 @@
 #include <stdio.h>
 #include <cs50.h>
 #include <string.h>
+#include <ctype.h> // for toupper
 
 int main(void) {
-    string name = get_string();
-    printf("%s",name);
+    // string name = get_string();
+    string name = "wolfgang amadeus mozart";
+
+    // verify that input is not null
+    if (name != NULL) {
+        // establishes demarcation between names
+        bool spacetrip = true;
+        // loop through each letter of input
+        for (int i = 0, length = strlen(name); i < length; i++) {
+            if (spacetrip == true) {
+                printf("%c", toupper(name[i]));
+                spacetrip = false;
+            }
+            // if it's a space then the next character is an initial
+            if ((int) name[i] == 32) {
+                spacetrip = true;
+            }
+        }
+    }
+    printf("\n");
     return 0;
 }
