1b816b7ca552bd232f1e3d9a107df846133dfc33
Zack Hartmann <zackhartmann@gmail.com>
2017-10-31 16:52:13 +0000

    notes

--- Pset2/caesar/caesar.c
+++ Pset2/caesar/caesar.c
@@ -8,13 +8,33 @@
 int main(int argc, string argv[]) {
     // verify two input arguments
     // verify that 2nd argument is a number (the rotate quotient)
-    if (!(argc == 2 && isdigit(argv[1])) {
+    if (!(argc == 2)) {
         printf("Usage: ./caesar k\n");
         return 1;
     }
+
+    // the second argument is the rotation quotient
+    int cycle = atoi(argv[1]);
+
+    printf("plaintext:  ");
     string plaintext = get_string();
 
-    s
+    printf("ciphertext: ");
+    for (int i = 0, length = strlen(plaintext); i < length; i++) {
+        // non-alpha characters should be passed through unaltered
+        if (!(isalpha(plaintext[i]))) {
+            printf("%c", plaintext[i]);
+        } else {
+            if (isupper(plaintext[i])) {
+                // ASCII uppercase starts at 65 - modulo for bounding
+                printf("%c", ((plaintext[i] + cycle - 65) % 26) + 65);
+            } else {
+                // ASCII lowercase starts at 97
+                printf("%c", ((plaintext[i] + cycle - 97) % 26) + 97);
+            }
+        }
+    }
+
     printf("\n");
     return 0;
 }
