484c72e54243c2513ec54fae7b088628266e22ac
Zack Hartmann <zackhartmann@gmail.com>
2017-10-31 15:37:13 +0000

    caesar - first commit

---
+++ Pset2/caesar/caesar.c
@@ -0,0 +1,20 @@
+// caesar
+
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(int argc, string argv[]) {
+    // verify two input arguments
+    // verify that 2nd argument is a number (the rotate quotient)
+    if (!(argc == 2 && isdigit(argv[1])) {
+        printf("Usage: ./caesar k\n");
+        return 1;
+    }
+    string plaintext = get_string();
+
+    s
+    printf("\n");
+    return 0;
+}
