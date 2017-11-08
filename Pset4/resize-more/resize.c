65bf01160b315394265cac7d0e169e39906341cb
Zack Hartmann <zackhartmann@gmail.com>
2017-11-08 23:22:29 +0000

    Improved Usage Info

--- Pset4/resize-more/resize.c
+++ Pset4/resize-more/resize.c
@@ -41,12 +41,12 @@ int main(int argc, char *argv[])
             outfile = argv[3];
             break;
         case 3:
-            printf("Header Print Mode:\n");
+            printf("~: Header Print Mode :~\n");
             infile = argv[2];
             outfile = "/dev/null";
             break;
         case 2:
-            printf("Resize Algorithm Test Mode:\n");
+            printf("~: Algorithm Test Mode :~\n");
             infile = "large.bmp";
             outfile = "_test.bmp";
             break;
