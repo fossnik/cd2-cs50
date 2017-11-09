64ec56600701ded5e35acb9a70c3975758f37a37
Zack Hartmann <zackhartmann@gmail.com>
2017-11-09 16:32:47 +0000

    Prints location of all JPEG Magic Numbers

--- Pset4/recover/recover.c
+++ Pset4/recover/recover.c
@@ -8,7 +8,7 @@
 int main(int argc, char *argv[])
 {
     // ensure proper usage
-    if (argc != 1)
+    if (argc != 2)
     {
         fprintf(stderr, "Usage: ./recover infile\n");
         return 1;
@@ -23,7 +23,26 @@ int main(int argc, char *argv[])
         return 2;
     }
 
-    // 0xff
+    // seek through input file 512 bytes at a time until end
+    for (long byte = 0; fgetc(inptr) != 0xffffffff; byte += 512) {
+        _Bool jpeg = 0; // assume not a jpeg (_Bool is a c99 keyword)
+
+        // seek n*512 clusters from file beginning
+        fseek(inptr, byte, SEEK_SET);
+
+        // test magic number for jpeg (ff d8 ff & e0-ef)
+        if (fgetc(inptr) == 0xff)
+            if (fgetc(inptr) == 0xd8)
+                if (fgetc(inptr) == 0xff) {
+                    int fourth = fgetc(inptr);
+                    if (fourth >= 0xe0 && fourth <= 0xef)
+                        jpeg = 1;
+                }
+        // found jpeg magic number
+        if (jpeg) {
+            printf("JPEG exists at byte %ld \n", byte);
+        }
+    }
 
     // close infile
     fclose(inptr);
