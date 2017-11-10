4fb156b9e9d94a19ef9ffc6f7aad1d9f61c5059d
Zack Hartmann <zackhartmann@gmail.com>
2017-11-10 18:17:33 +0000

    WORKING./recover!

--- Pset4/recover/recover.c
+++ Pset4/recover/recover.c
@@ -7,77 +7,81 @@
 
 static const int block_size = 512;
 
-int main(int argc, char *argv[])
+// int main(int argc, char *argv[])
+// {
+//     // ensure proper usage
+//     if (argc != 2)
+//     {
+//         fprintf(stderr, "Usage: ./recover infile\n");
+//         return 1;
+//     }
+
+//     // open input file
+//     FILE *inptr = fopen(argv[1], "r");
+//     if (inptr == NULL)
+//     {
+//         fprintf(stderr, "Could not open %s.\n", argv[1]);
+//         return 2;
+//     }
+int main(void)
 {
-    // ensure proper usage
-    if (argc != 2)
-    {
-        fprintf(stderr, "Usage: ./recover infile\n");
-        return 1;
-    }
-    char *infile = argv[1];
+    FILE *inptr = fopen("card.raw", "r");   // open input file
 
-    // open input file
-    FILE *inptr = fopen(infile, "r");
-    if (inptr == NULL)
-    {
-        fprintf(stderr, "Could not open %s.\n", infile);
-        return 2;
-    }
-
-    fseek(inptr, 0, SEEK_END);      // point to file end
-    long raw_size = ftell(inptr);   // record position of end (ie filesize)
+    int fread_buffer[block_size];   // buffer for blocks IO
 
     int file_count = 0; // counter to keep track of filenames
 
     // iterate through input file one block at a time
-    for (long byte = 0; byte < raw_size; byte += block_size)
+    for (long byte = 0; fgetc(inptr) != 0xffffffff; byte += block_size)
     {
         _Bool signature_detected = 0; // (_Bool is a c99 keyword)
+        int fourth;
 
         // set pointer location by distance from file start
         fseek(inptr, byte, SEEK_SET);
 
         // test magic number for jpeg (ff d8 ff & e0-ef)
-        if (fgetc(inptr) == 0xff)
-            if (fgetc(inptr) == 0xd8)
-                if (fgetc(inptr) == 0xff) {
-                    int fourth = fgetc(inptr);
-                    if (fourth >= 0xe0 && fourth <= 0xef)
-                        signature_detected = 1; // "magic" numbers detected!
-                }
+        if (fgetc(inptr) == 0xff & fgetc(inptr) == 0xd8 & fgetc(inptr) == 0xff)
+        {
+            fourth = fgetc(inptr);
+            if (fourth >= 0xe0 && fourth <= 0xef)
+                signature_detected = 1; // "magic" numbers detected!
+        }
 
         if (signature_detected)
         {
-            fseek(inptr, byte, SEEK_SET); // reset pointer
-            char fn_buffer[3];  // buffer for filenames
+            fseek(inptr, byte, SEEK_SET);   // reset pointer
+            char fn_buffer[8];              // buffer for filenames
             // write into buffer a filename for the found jpeg
             sprintf(fn_buffer,"%003d.jpg", file_count);
-            fprintf(stderr, "%s | bytes: %ld - ", fn_buffer, ftell(inptr));
+
+            // DEBUG
+            fprintf(stderr, "%s | byte: %ld - ", fn_buffer, ftell(inptr));
 
             // open output file
-            // FILE *outptr = fopen("/dev/null", "w");
             FILE *outptr = fopen(fn_buffer, "w");
 
-            // write output file while its not empty.
-            while (fgetc(inptr) != 0xffffffff)
+            // write output file while input not NULL.
+            while (fread(fread_buffer, block_size, 1, inptr) != 0)
             {
+                fwrite(fread_buffer, block_size, 1, outptr);
+
                 byte += block_size;
-                fseek(inptr, byte, SEEK_SET); // reset pointer
-
-                fwrite(&inptr, block_size, 1, outptr); // write one block to outfile
-
-                // prophylacticly test against signature of next file
+                fseek(inptr, byte, SEEK_SET); // reset pointer.
+                // avoid running over the next file
                 if (fgetc(inptr) == 0xff) if (fgetc(inptr) == 0xd8) if (fgetc(inptr) == 0xff)
                 {
-                    int fourth = fgetc(inptr);
+                    fourth = fgetc(inptr);
                     if (fourth >= 0xe0 && fourth <= 0xef)
                     {
+                        // DEBUG - Where new file begins
                         fprintf(stderr, "%ld\n", ftell(inptr) - 4);
-                        byte -= block_size; // counteracting for loop incrementation.
+
+                        byte -= block_size; // counteract increment
                         break; // end upon encounter of magic numbers
                     }
                 }
+                fseek(inptr, byte, SEEK_SET); // reset pointer.
             }
             file_count++;
             fclose(outptr); // close outfile - end of jpeg
