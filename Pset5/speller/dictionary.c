0366d94eb2f39a26d85de224716aca373db139ad
Zack Hartmann <zackhartmann@gmail.com>
2017-11-18 06:42:29 +0000

    Improved Annotations

--- Pset5/speller/dictionary.c
+++ Pset5/speller/dictionary.c
@@ -13,11 +13,11 @@
 // define the number of hashtable "buckets" (Singly Linked Lists)
 static const unsigned int hash_buckets = 5;
 
-// wordcount counter variable
+// wordcount tracker
 unsigned int wc = 0;
 
-// create an array to store locations of HEAD node of each singly linked list
-// (node struct is defined in dictionary.h)
+// this node array stores the location of each node HEAD
+// (node struct is type defined in dictionary.h)
 node *sll_head_table[hash_buckets];
 
 /**
@@ -25,8 +25,8 @@ node *sll_head_table[hash_buckets];
  */
 bool check(const char *word)
 {
-    // create a lowercase version of the test word using node struct
-    // while const char *word is read only, a node is mutable
+    // while the given const char *word is read-only, a node struct is mutable
+    // a temporary node shall store the small-case transmogrification of 'word'
     node *temp_node = malloc(sizeof(node));
 
     // convert test word to small case
@@ -36,9 +36,7 @@ bool check(const char *word)
     // hash input to determine which bucket to search through
     unsigned int bucket = hasher(word);
 
-    // "traversing linked lists" - seeking word
-    // "node" is a struct defined in a dictionary.h typedef
-    // iterate through all the words in the bucket, comparing strings
+    // seeking 'word' match, parse each word element in the selected node chain
     for (node* dict_node = sll_head_table[bucket]; dict_node != NULL; dict_node = dict_node->next)
     {
         printf("WORD: %s    \ttemp->WORD: %s    \tdict->WORD: %s    \t(buckets: %d %d %d)\n", word, temp_node->word, dict_node->word, hasher(word), hasher(dict_node->word), hasher(temp_node->word));
@@ -67,14 +65,14 @@ bool load(const char *dictionary)
     }
 
     // create a buffer for storing individual words
-    char word[LENGTH];
+    char a_word[LENGTH];
 
-    // set the heads of each to null.
+    // set NULL each index in the hash table, signifying absence of list HEADs
     for (int i = 0; i < hash_buckets; i++)
         sll_head_table[i] = NULL;
 
-    // scan dictionary file line by line (ie. word by word)
-    while( fscanf(dict_p, "%s", word) != EOF )
+    // parse dictionary file line by line (ie. word by word)
+    while( fscanf(dict_p, "%s", a_word) != EOF )
     {
         // malloc a node pointer for each new word
         node *new_node = malloc(sizeof(node));
@@ -86,30 +84,32 @@ bool load(const char *dictionary)
             return false;
         }
 
-        // convert to lower case
-        for (int i = 0; word[i] != '\0'; i++)
-            word[i] = tolower(word[i]);
+        // convert to lower case (mutable array)
+        for (int i = 0; a_word[i] != '\0'; i++)
+            a_word[i] = tolower(a_word[i]);
 
         // associate this word with the new node's word element (string copy)
-        strcpy(new_node->word, word);
+        strcpy(new_node->word, a_word);
 
         // hash the word (key) to determine which bucket ought keep the node
         unsigned int bucket = hasher(new_node->word);
 
         // Mediation of Hash Collisions
-        // if empty - set the new node's NEXT element pointing to NULL, and
-        // register the new_node in our array of s-linked list HEAD nodes
+        // if HEAD empty, set the new node's NEXT element pointing to NULL, and
+        // register the new_node in the array of linked list HEADs
         if (sll_head_table[bucket] == NULL)
         {
             new_node->next = NULL;
             sll_head_table[bucket] = new_node;
         }
         // otherwise, the whole chain gets tethered to new_node's NEXT property.
-        // new_node is then registered as the new HEAD of the singly linked list
+        // new_node is then registered as the new HEAD of the linked list
         else
         {
             new_node->next = sll_head_table[bucket];
             sll_head_table[bucket] = new_node;
+
+            // DEBUG: Print words entered into dictionary
             printf("PASS: %s\n", new_node->word);
             return true;
         }
@@ -145,10 +145,10 @@ unsigned int hasher(const char *word)
 {
     unsigned int hash_val = 1;
 
-    // loop through each letter and perturb.
-    // it's pretty random, but probably not random enough for certain uses.
-    // most significantly, the correlation between function input and output
-    // is fixed (returns the same result every time)
+    // derive a numerical output that's contingent on each letter of the input.
+    // for some use cases, this type of hash function is insufficiently random,
+    // but, most significantly, the fixed correlation between input and output
+    // ensures that a given word input always returns the same number as output
     for (int i = 0; word[i] != '\0'; i++)
         hash_val ^= word[i];
 
